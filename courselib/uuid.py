e930d92e5ba02698a636f4e6afde3c5072fd2bcf
Greg Baker <ggbaker@sfu.ca>
2026-07-07 19:32:43 -0700

    add note about safe deployment

--- courselib/uuid.py
+++ courselib/uuid.py
@@ -8,6 +8,10 @@ from django.db import models
 # Django 5 on mariadb: field type is UUID, matching this, for seamless transition.
 # Once cleanly on Django 5, fields may be reverted to models.UUIDField and this file can be discarded.
 
+# Experimentally, a test system worked throughout this:
+# (0) have existing data (1) merge this in with its migrations (2) make new-code (3) make migrate-safe 
+# ... so the mariadb treatment of CHAR(32) and UUID must be close enough either way for django.
+
 class UUIDField(models.UUIDField):
     def db_type(self, connection):
        return "uuid"
